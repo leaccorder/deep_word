0887bcd0ccb6d7a08b01d1e7a0434d6fbf735647
leaccorder <lea.pistaches@gmail.com>
2026-04-18 23:10:56 +0200

    module0

---
+++ module0/ex01/player.py
@@ -0,0 +1,3 @@
+print("Bonjour !")
+print("Je m'apelle Ela")
+print("J'ai 100HP.")
---
+++ module1/ex03/player_etat.py
@@ -0,0 +1,7 @@
+hp = int(input("HP : "))
+if hp > 66:
+    print("En bonne santé.")
+elif hp >= 33:
+    print("Blessé.")
+else:
+    print("Etat critique.")
